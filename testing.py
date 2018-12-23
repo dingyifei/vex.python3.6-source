ddc3d3003620af3a0449ecfb2ba48a4550631a62
Yifei Ding <dingyifeiair@gmail.com>
2018-12-23 12:33:35 +0800

    get ready for unit test

--- testing.py
+++ testing.py
@@ -1,8 +1,25 @@
 import sys
 import json
 import vexdb_json
+import unittest
+
 a = open("testing.json")
 
 a_json = json.loads(a.read())
 print(a_json)
 print(vexdb_json.filter_info(a_json, "number", "program"))
+
+
+
+class IntegerArithmeticTestCase(unittest.TestCase):
+    def testAdd(self):  # test method names begin with 'test'
+        self.assertEqual((1 + 2), 3)
+        self.assertEqual(0 + 1, 1)
+
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+
+
+if __name__ == '__main__':
+    unittest.main()
